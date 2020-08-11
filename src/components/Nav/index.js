091ac7be165b6cdbe028c25a862bd2a2d70d5ef4
mattkohl82 <mattkohl82@gmail.com>
2020-08-11 14:28:20 -0500

    highlighted nav element on active

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 import Nav from 'react-bootstrap/Nav';
 
@@ -7,34 +7,32 @@ function Navigation() {
 
     return (
     
-            <Nav className="mr-auto">
-                <Link className='nav-link' to="/about">
-                    <p className='navcolor'>
-                        <img src="https://img.icons8.com/nolan/50/about.png" alt='about' />
-                        About
-                    </p>
-                </Link>
-                <Link className='nav-link' to="/portfolio">
-                    <p className='navcolor'>
-                        <img src="https://img.icons8.com/nolan/50/contract-job.png" alt='portfolio'/>
-                        Portfolio
-                    </p>
-                </Link>
-                <Link className='nav-link' to="/contact">
-                    <p className='navcolor'>
-                        <img src="https://img.icons8.com/nolan/50/contact-card.png" alt="contact"/>
-                        Contact
-                    </p>
-                </Link>
-                <Link className='nav-link' to="/resume">
-                    <p className='navcolor'>
-                        <img src="https://img.icons8.com/nolan/50/resume.png" alt="resume"/>
-                        Resume
-                    </p>
-                </Link>
-            </Nav>
-
-
+        <Nav variant="pills" defaultActiveKey="/about" className="row navigation" id="navigation">
+            <Nav.Link className='nav-link' href="/mattkohl82/#/about">
+                <p className='navcolor'>
+                    <img src="https://img.icons8.com/nolan/50/about.png" alt='about' />
+                    About
+                </p>
+            </Nav.Link>
+            <Nav.Link className='nav-link' eventKey="/portfolio" href="/mattkohl82/#/portfolio">
+                <p className='navcolor'>
+                    <img src="https://img.icons8.com/nolan/50/contract-job.png" alt='portfolio'/>
+                    Portfolio
+                </p>
+            </Nav.Link>
+            <Nav.Link className='nav-link' eventKey="/contact" href="/mattkohl82/#/contact">
+                <p className='navcolor'>
+                    <img src="https://img.icons8.com/nolan/50/contact-card.png" alt="contact"/>
+                    Contact
+                </p>
+            </Nav.Link>
+            <Nav.Link className='nav-link' eventKey="/resume" href="/mattkohl82/#/resume">
+                <p className='navcolor'>
+                    <img src="https://img.icons8.com/nolan/50/resume.png" alt="resume"/>
+                    Resume
+                </p>
+            </Nav.Link>
+        </Nav>
     )
 }
 
